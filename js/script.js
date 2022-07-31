6c7dbb2b4c127d3c6a887bcbc36cc910fe36e7b8
Hasan <programmerhasan.bd@gmail.com>
2022-08-01 05:22:59 +0600

    complete layout and selected id in js file to make dynamic

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// All selectors
+const playerOne = document.getElementById("playerOne");
+const playerTow = document.getElementById("playerTwo");
+const resetBtn = document.getElementById("resetBtn");
+
+const inputPlayingTarget = document.getElementById("inputPlayingTarget");
+let playingTarget = document.getElementById("playingTarget");
+
+let p1score = document.getElementById("p1score");
+let p2score = document.getElementById("p2score");
